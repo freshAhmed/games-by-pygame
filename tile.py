3827c0ccfd999471794d557bca9bc0e43b7237e5
Ahmed ali <sertyd37@gmail.com>
2025-10-17 16:41:29 -0300

    update tile.py to code base

--- tile.py
+++ tile.py
@@ -20,13 +20,15 @@ def joinimagetile(pos,size,angle,images):
  return suface
 
 class Tile(pygame.sprite.Sprite):
- def __init__(self,pos,imagepartes_tile,size,pips):
+ def __init__(self,pos,imagestiles,size,pips):
   
   pygame.sprite.Sprite.__init__(self)
   self.pips=pips
   self.pos=pygame.Vector2(pos)
-  self.imagespartes =imagepartes_tile
-  self.image=joinimagetile(pos,size,0,self.imagespartes[1])
+  self.imagestiles =imagestiles
+  topimage=self.imagestiles[self.pips[0]]
+  bottomimage=self.imagestiles[self.pips[1]]
+  self.image=joinimagetile(pos,size,0,[topimage,bottomimage])
   self.rect=self.image.get_rect(center=self.pos)
   self.angle=0
   self.flag=False #to control what tile allowed to play
@@ -40,8 +42,9 @@ class Tile(pygame.sprite.Sprite):
   
  def render(self):
   width,height=self.size
-  
-  image=joinimagetile(self.pos,self.size,self.angle,self.imagespartes[1])
+  topimage=self.imagestiles[self.pips[1]]
+  bottomimage=self.imagestiles[self.pips[0]]
+  image=joinimagetile(self.pos,self.size,self.angle,[topimage,bottomimage])
   if self.angle==0:
    surface=pygame.Surface((width,height))
    surface.blit(image,(0,0),(0,0,width,height))
@@ -50,8 +53,9 @@ class Tile(pygame.sprite.Sprite):
    surface.blit(image,(0,0),(0,0,height,width))
   self.image=surface
   self.rect=self.image.get_rect(center=self.pos)
+
  def checkpips_and_angle(self):
-  leftpip,rightpip=self.pips
+  leftpip,rightpip=self.getpips()
   if leftpip==rightpip:
    self.angle=0
   else:
@@ -67,7 +71,7 @@ class Tile(pygame.sprite.Sprite):
 
  
  def getpips(self):
-  return self.pips['leftpip'],self.pips['rightpip']
+  return self.pips
  def updatesize(self):
   self.width,self.height=self.rect.size
  def getpos(self):
@@ -79,16 +83,16 @@ class Tile(pygame.sprite.Sprite):
   else:
    self.angle=0
  def rotate(self,pips,side):
-  imagespartes_meta=self.imagespartes[0]
-  imagespartes_info=self.imagespartes[1]
-  # log.info(imagespartes_meta)
-  if pips[0] is None:
-   return 
-  if side == 'left':
-   if pips[0] == imagespartes_meta[0]:
-    self.imagespartes[1]=tuple(reversed(imagespartes_info))
-    self.imagespartes[0]=tuple(reversed(imagespartes_meta))
-  elif side =='right':
-   if pips[1]== imagespartes_meta[1]:
-    self.imagespartes[1]=tuple(reversed(imagespartes_info))
-    self.imagespartes[0]=tuple(reversed(imagespartes_meta))
+  tilepips=self.getpips()
+  previousepips=tuple(self.getpips())
+  if side=='right':
+   if pips[1]!=tilepips[0]:
+     self.pips=tuple(reversed(self.pips))
+     
+  elif side=='left':
+    if pips[0]!=tilepips[1] :
+     self.pips=tuple(reversed(self.pips))
+  else:
+   pass
+  log.info(f'the tile pips is {self.pips} and previouspips is {previousepips} and arean pips is {pips}')
+  
