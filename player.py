813766d57f101399d319461974d6732a51a8a094
Ahmed ali <sertyd37@gmail.com>
2025-10-22 16:28:18 -0300

    update player.py file

--- player.py
+++ player.py
@@ -10,24 +10,38 @@ class Deck(pygame.sprite.Group):
   self.background=background
   self.image=pygame.Surface(size,pygame.SRCALPHA)
   self.rect=self.image.get_rect(center=self.pos)
-
+  self.scale=0.8
+  self.dectpips=[]
  def render(self,screen):
-  pygame.draw.rect(screen,self.background,self.rect)
+  # pygame.draw.rect(screen,self.background,self.rect)
   self.draw(screen)
 
  def addtile(self,tile):
+  # self.add(tile)
+  tiles=self.sprites()
+  xoffset,yoffset=tiles[-1:][0].pos
+  tile.updatepos((xoffset+tile.size[0],yoffset))
+
+
+  tile.updatescale(self.scale) 
   self.add(tile)
  
  def addtiles(self, tiles):
-  offsetx=10
+  offsetx=self.rect.centerx//1.8
   offsety=self.rect.centery
   for tile in tiles:
-   offsetx+=self.margen[0]*12
+   offsetx+=self.margen[0]*5
+   self.dectpips.append(tile.getpips())
    tile.updatepos((offsetx,offsety))
+   tile.updatescale(self.scale) 
 
   self.add(tiles)
  
  def removetile(self,tile):
+  tilepips=tile.getpips() if tile.getpips() in self.dectpips else tuple(reversed(tile.getpips()))
+  for pip in self.dectpips:
+   if (pip[0]==tilepips[0] and pip[1]==tilepips[1] ) or (pip[0]==tilepips[1] and pip[1]==tilepips[0]):
+    self.dectpips.remove(pip)
   self.remove(tile)
  
  def chosetile(self,pos):
@@ -54,4 +68,11 @@ class Player():
   self.deck.update(*args,**kwargs)
   self.deck.render(screen)
 
+ def checkpips(self,currentpips):
+  if currentpips[0]==None:
+   return True 
+  for pip in self.deck.dectpips:
+    if (pip[0]==currentpips[0] or pip[1]==currentpips[0] )or (pip[0]==currentpips[1] or pip[1]==currentpips[1] ):
+     return True
+  return False 
 
